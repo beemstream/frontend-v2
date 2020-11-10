ae280f5bcdb713c558743224c1e83f1f46c3e208
Ibrahim M <dev@beemstream.com>
2020-11-10 21:10:07 +0000

    chore: added routing, lazy home route

---
+++ apps/platform/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule,
+  ]
+})
+export class AppRoutingModule { }
