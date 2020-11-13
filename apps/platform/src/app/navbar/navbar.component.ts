5328ca29d7785e59103f310e2a9bd7b2ce1f0daa
Reha Begum <rehabegum@Rehas-MacBook-Air.local>
2020-11-13 23:24:25 +0000

    feat: added navbar template

---
+++ apps/platform/src/app/navbar/navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'frontend-v2-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
