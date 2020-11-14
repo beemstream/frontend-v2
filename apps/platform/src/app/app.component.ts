833dc90cbe4f2c63edde1e96b07a32caa9de4a74
Reha Begum <rehabegum@Rehas-MacBook-Air.local>
2020-11-14 18:46:03 +0000

    feat: update navbar styles

--- apps/platform/src/app/app.component.ts
+++ apps/platform/src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'frontend-v2-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'platform';
