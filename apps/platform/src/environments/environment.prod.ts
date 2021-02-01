0bd549232d759cf8604b8ede4c8e0b57f06da1e7
BeemStream <beemstreamofficial@gmail.com>
2021-02-01 12:29:47 +0000

    add https

--- apps/platform/src/environments/environment.prod.ts
+++ apps/platform/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  streamCollectionUrl: 'http://beemstream.com/stream-collection'
+  streamCollectionUrl: 'https://beemstream.com/stream-collection'
 };
