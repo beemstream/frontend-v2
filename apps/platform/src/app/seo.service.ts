e6090e9e4591aa0a67ac199396d0a196ba98b07c
BeemStream <beemstreamofficial@gmail.com>
2021-02-26 22:26:02 +0000

    chore: add seo service

---
+++ apps/platform/src/app/seo.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { Meta, MetaDefinition, Title } from '@angular/platform-browser';
+
+export enum SeoTag {
+  Image = 'og:image',
+  Url = 'og:url',
+  SiteName = 'og:sitename',
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SeoService {
+  constructor(
+    private pageTitle: Title,
+    private meta: Meta,
+  ) {}
+
+  addTitle(title: string) {
+    this.pageTitle.setTitle(title);
+    this.meta.addTag({ 'og:title': title });
+    return this;
+  }
+
+  addDescription(description: string) {
+    this.meta.addTag({ 'og:description': description });
+    return this;
+  }
+
+  addMeta(tags: MetaDefinition[]) {
+    this.meta.addTags(tags);
+    return this;
+  }
+}
