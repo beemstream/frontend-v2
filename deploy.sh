9bc6cd51e614602fba1477ae76d80e7aff45b9d8
BeemStream <beemstreamofficial@gmail.com>
2021-03-10 00:31:42 +0000

    0.4.0

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,9 @@ PACKAGE_VERSION=`node -p "require('./package.json').version"`
 docker build -t beemstream/nbp-frontend:$PACKAGE_VERSION .
 docker push beemstream/nbp-frontend:$PACKAGE_VERSION
 
-IMAGE_DIGEST=`docker image inspect --format='{{index .Id}}' beemstream/nbp-frontend:$PACKAGE_VERSION`
 # Tag image as latest
 docker pull beemstream/nbp-frontend:$PACKAGE_VERSION
+IMAGE_DIGEST=`docker image inspect --format='{{index .Id}}' beemstream/nbp-frontend:$PACKAGE_VERSION`
 docker tag beemstream/nbp-frontend:$IMAGE_DIGEST beemstream/nbp-frontend:latest
 docker push beemstream/nbp-frontend:latest
 
