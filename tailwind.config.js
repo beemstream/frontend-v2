2625a79668dc9ba637d85e7404062e4c2ca88259
BeemStream <beemstreamofficial@gmail.com>
2021-05-15 02:03:35 +0100

    feat: update tailwind, remove custom-webpack, deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
+const { guessProductionMode } = require('@ngneat/tailwind');
+
+process.env.TAILWIND_MODE = guessProductionMode() ? 'build' : 'watch';
+
 module.exports = {
   prefix: '',
+  mode: 'jit',
   purge: {
-    content: ['./apps/**/*.{html,ts}', './libs/**/*.{html,ts}'],
+    content: [
+      './apps/**/*.{html,ts,css,scss,sass,less,styl}',
+      './libs/**/*.{html,ts,css,scss,sass,less,styl}',
+    ],
   },
   darkMode: 'class', // or 'media' or 'class'
   theme: {
