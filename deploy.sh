949325a52d1069acdd75e321d8097915a42947ea
BeemStream <beemstreamofficial@gmail.com>
2021-06-06 01:44:58 +0100

    feat: clean up container and images after deploy

--- deploy.sh
+++ deploy.sh
@@ -12,5 +12,4 @@ docker pull beemstream/nbp-frontend:$PACKAGE_VERSION
 docker tag beemstream/nbp-frontend:$PACKAGE_VERSION beemstream/nbp-frontend:latest
 docker push beemstream/nbp-frontend:latest
 
-ssh root@157.245.43.172 "docker service update --image beemstream/nbp-frontend beemstream_nbp_frontend"
-ssh root@157.245.43.172 "docker image prune -a -f"
+ssh root@157.245.43.172 "docker service update --image beemstream/nbp-frontend beemstream_nbp_frontend && docker container prune -f && docker image prune -a -f"
