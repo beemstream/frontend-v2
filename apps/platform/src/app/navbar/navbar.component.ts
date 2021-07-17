63f8b1c46fea37d5f3d943265c4ac3a8bcba3fc4
BeemStream <beemstreamofficial@gmail.com>
2021-07-17 22:53:16 +0100

    fix: use twitchOauthUrl from environment

--- apps/platform/src/app/navbar/navbar.component.ts
+++ apps/platform/src/app/navbar/navbar.component.ts
@@ -8,6 +8,7 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { Observable, Subscription } from 'rxjs';
 import { share, tap } from 'rxjs/operators';
+import { environment } from '../../environments/environment';
 import {
   TwitchOauthService,
   TwitchValidateToken,
@@ -26,6 +27,7 @@ export class NavbarComponent implements OnDestroy {
   faGift = faGift;
   faSignOutAlt = faSignOutAlt;
   logoutSubscription: Subscription | undefined;
+  twitchOauthUrl = environment.twitchOauthUrl;
 
   get icon() {
     return this.isNavActive ? this.faTimes : this.faBars;
