a9a49c9397a8a78fa7d51a21877dcbb7d368d7c5
BeemStream <beemstreamofficial@gmail.com>
2021-10-15 22:38:15 +0100

    chore: remove old filter convention

--- apps/platform/src/app/filter-stream-list/attribute-filters/index.ts
+++ apps/platform/src/app/filter-stream-list/attribute-filters/index.ts
@@ -1,35 +1,3 @@
-import {
-  StreamLanguageProvider,
-  StreamListProvider,
-  StreamFilteredLanguageProvider,
-  SearchTermProvider,
-  StreamProgrammingLanguageProvider,
-} from '../stream-filter.provider';
-import { UserFollowsProvider } from './user-follows.provider';
-import { MarathonRunnersProvider } from './marathon-runners.provider';
-import { MostPopularProvider } from './most-popular.provider';
-import { NeedsLoveProvider } from './needs-love.provider';
-import { SlowStartersProvider } from './slow-starters.provider';
-
-export * from './most-popular.provider';
-export * from './needs-love.provider';
-export * from './marathon-runners.provider';
-export * from './slow-starters.provider';
-export * from './user-follows.provider';
-
-export const StreamQueryFilters = [
-  StreamLanguageProvider,
-  StreamProgrammingLanguageProvider,
-  StreamListProvider,
-  SearchTermProvider,
-  StreamFilteredLanguageProvider,
-  MostPopularProvider,
-  NeedsLoveProvider,
-  MarathonRunnersProvider,
-  SlowStartersProvider,
-  UserFollowsProvider,
-];
-
 export * from './filterByCategory';
 export * from './filterByLanguage';
 export * from './filterByProgrammingLanguages';
--- apps/platform/src/app/filter-stream-list/filter-stream.module.ts
+++ apps/platform/src/app/filter-stream-list/filter-stream.module.ts
@@ -7,8 +7,6 @@ import {
   StreamCardModule,
 } from '@frontend-v2/shared-ui';
 import { FiltersModule } from '../filters/filters.module';
-import { StreamFilterService } from '../stream-filter.service';
-import { StreamQueryFilters } from './attribute-filters';
 import { FilterStreamListComponent } from './filter-stream-list.component';
 
 @NgModule({
@@ -21,7 +19,6 @@ import { FilterStreamListComponent } from './filter-stream-list.component';
     FiltersModule,
     ButtonModule,
   ],
-  providers: [StreamFilterService, StreamQueryFilters],
   exports: [FilterStreamListComponent],
 })
 export class FilterStreamListModule {}
